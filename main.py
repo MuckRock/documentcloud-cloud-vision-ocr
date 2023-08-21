ba1dbac24a18452b960f40247ad773a1ae541a5b
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-08-21 08:06:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ class CloudVision(AddOn):
         super().__init__(*args, **kwargs)
         self.setup_credential_file()
         # Set bucket name
-        bucket_name = 'documentcloud_cloud_vision_ocr'
+        self.bucket_name = 'documentcloud_cloud_vision_ocr'
         # Instantiate a client for the client libraries 'storage' and 'vision'
-        storage_client = storage.Client()
-        vision_client = vision.ImageAnnotatorClient()
-        bucket = storage_client.get_bucket(bucket_name)
+        self.storage_client = storage.Client()
+        self.vision_client = vision.ImageAnnotatorClient()
+        self.bucket = storage_client.get_bucket(bucket_name)
         # Activate DOCUMENT_TEXT_DETECTION feature
-        feature = vision.Feature(type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
+        self.feature = vision.Feature(type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
         # Set file format to PDF
-        mime_type = 'application/pdf'
+        self.mime_type = 'application/pdf'
         # The number of pages that will be grouped in each json response file
-        batch_size = 1
+        self.batch_size = 1
     def setup_credential_file(self):
         """Sets up Google Cloud credential file"""
         credentials = os.environ["TOKEN"]
