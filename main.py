a63029c6d78e96d7dcacf3c24f892296a24b823d
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-08-21 07:21:49 -0500

    Draft 1

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ This is Add-On allows users to use Google Cloud Vision API to OCR a document.
 """
 
 import os
+import json
+import glob
 import requests
 from documentcloud.addon import AddOn
 from listcrunch import uncrunch
 from tempfile import NamedTemporaryFile
+from google.cloud import vision
+from google.cloud import storage
 
 class CloudVision(AddOn):
     """OCR your documents using Google Cloud Vision API"""
+    bucket_name = 'documentcloud_cloud_vision_ocr'
+    # Instantiate a client for the client libraries 'storage' and 'vision'
+    vision_client = vision.ImageAnnotatorClient()
+    bucket = storage_client.get_bucket(bucket_name)
+    # Activate DOCUMENT_TEXT_DETECTION feature
+    feature = vision.Feature(type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
+    # Set file format to PDF
+    mime_type = 'application/pdf'
+    # The number of pages that will be grouped in each json response file
+    batch_size = 1
+
     def setup_credential_file(self):
         """Sets up Google Cloud credential file"""
         credentials = os.environ["TOKEN"]
@@ -52,50 +67,87 @@ class CloudVision(AddOn):
         self.set_message(f"There are {num_pages} pages in this document set. It would cost {num_pages} AI credits to OCR this document set.")
         sys.exit(0)
 
-    def main(self):
-        for document in self.get_documents():
-            # get the dimensions of the pages
-            page_spec = [map(float, p.split("x")) for p in uncrunch(document.page_spec)]
-            if document.access != "public":
-                self.set_message("Document must be public")
-                return
-            data = {
-                "url": document.pdf_url,
-                "isOverlayRequired": True,
-                "language": document.language,
-            }
-            resp = requests.post(URL, headers={"apikey": os.environ["KEY"]}, data=data)
-            results = resp.json()
-            if results["IsErroredOnProcessing"]:
-                self.set_message(f"Error")
-                return
-            pages = []
-            for i, (page_results, (width, height)) in enumerate(
-                zip(results["ParsedResults"], page_spec)
-            ):
-                # ocrspace dimensions need a correction factor for some reason
-                width *= (4/3)
-                height *= (4/3)
+    def JSON_OCR(self, input_dir, filename):
+        #Create a remote path. The combination of os.path.basename and os.path.normath extracts the name of the last directory of the path, i.e. 'docs_to_OCR'. 
+        remote_subdir= os.path.basename(os.path.normpath(input_dir))
+        rel_remote_path = os.path.join(remote_subdir, filename)
 
-                page = {
-                    "page_number": i,
-                    "text": page_results["ParsedText"],
-                    "ocr": "ocrspace1",
-                    "positions": [],
-                }
-                for line in page_results["TextOverlay"]["Lines"]:
-                    for word in line["Words"]:
-                        page["positions"].append(
-                            {
-                                "text": word["WordText"],
-                                "x1": (word["Left"] ) / width,
-                                "y1": (word["Top"] ) / height,
-                                "x2": (word["Left"] + word["Width"] ) / width,
-                                "y2": (word["Top"] + word["Height"] ) / height,
-                            }
-                        )
+        #Upload file to Google Cloud Bucket as a blob. 
+        blob = bucket.blob(rel_remote_path)
+        blob.upload_from_filename(os.path.join(input_dir, filename))
+
+        #Remote path to the file.
+        gcs_source_uri = os.path.join('gs://', bucket_name, rel_remote_path)
+
+        #Input source and input configuration.
+        gcs_source = vision.GcsSource(uri=gcs_source_uri)
+        input_config = vision.InputConfig(gcs_source=gcs_source, mime_type=mime_type)
+
+        #Path to the response JSON files in the Google Cloud Storage. In this case, the JSON files will be saved inside a subfolder of the Cloud version of the input_dir called 'json_output'.
+        gcs_destination_uri = os.path.join('gs://', bucket_name, remote_subdir, 'json_output', filename[:30]+'_')
+
+        #Output destination and output configuration.
+        gcs_destination = vision.GcsDestination(uri=gcs_destination_uri)
+        output_config = vision.OutputConfig(gcs_destination=gcs_destination, batch_size=batch_size)
+
+        #Instantiate OCR annotation request.
+        async_request = vision.AsyncAnnotateFileRequest(
+        features=[feature], input_config=input_config, output_config=output_config)
+
+        #The timeout variable is used to dictate when a process takes too long and should be aborted. If the OCR process fails due to timeout, you can try and increase this threshold.
+        operation = vision_client.async_batch_annotate_files(requests=[async_request])
+        operation.result(timeout=360)
+
+        return gcs_destination_uri
+
+    def list_blobs(gcs_destination_uri):
+        #Identify the 'prefix' of the response JSON files, i.e. their path and the beginning of their filename.
+        prefix='/'.join(gcs_destination_uri.split('//')[1].split('/')[1:])
+
+        #Use this prefix to extract the correct JSON response files from your bucket and store them as 'blobs' in a list.
+        blobs_list = list(bucket.list_blobs(prefix=prefix))
+
+        #Order the list by length before sorting it alphabetically so that the text appears in the correct order in the output file (i.e. so that the first two items of the list are 'output-1-to-2.json' and 'output-2-to-3.json' instead 'output-1-to-2.json' and 'output-10-to-11.json', as produced by the default alphabetical order).
+        blobs_list = sorted(blobs_list, key=lambda blob: len(blob.name))
+
+        return blobs_list
+
+    def set_doc_text(self, blobs_list):
+        pages = []
+        for i, blob in enumerate(blobs_list):
+            json_string = blob.download_as_string()
+            response=json.loads(json_string)
+            full_text_response = response['responses']
+            for text in full_text_response:
+                try:
+                    annotation=text['fullTextAnnotation']
+                    page = {
+                        "page_number": i,
+                        "text": annotation['text'],
+                        "ocr": "googlecv",
+                        "positions": [],
+                    }
+                except:
+                    pass
                 pages.append(page)
-            self.client.patch(f"documents/{document.id}/", {"pages": pages})
+        self.client.patch(f"documents/{document.id}/", {"pages": pages})
+       
+
+    def vision_method(self, document, input_dir, filename):
+        #Assign the remote path to the response JSON files to a variable.
+        gcs_destination_uri = self.JSON_OCR(input_dir, filename)
+        #Create an ordered list of blobs from these remote JSON files.
+        blobs_list = self.list_blobs(gcs_destination_uri)
+        self.set_doc_text(blobs_list)
+    
+    def main(self):
+        os.mkdir("out")
+        for document in self.get_documents():
+            pdf_name = f"'{document.title}.pdf'"
+            with open(f"./out/{document.title}.pdf", "wb") as file:
+                file.write(document.pdf)
+            self.vision_method(document, "out", pdf_name)
+            
 
 if __name__ == "__main__":
     CloudVision().main()
