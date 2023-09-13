472a8029e1c854b8a0b96a759ae1feb802222bc9
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-09-13 05:21:31 -0500

    Add call to validate()

--- main.py
+++ main.py
@@ -214,6 +214,7 @@ class CloudVision(AddOn):
     def main(self):
         """For each document, it sends the PDF to Google Cloud Storage and runs OCR"""
         os.mkdir("out")
+        self.validate()
         for document in self.get_documents():
             pdf_name = f"{document.title}.pdf"
             with open(f"./out/{document.title}.pdf", "wb") as file:
